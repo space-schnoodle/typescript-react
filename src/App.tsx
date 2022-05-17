4f817e061ebdfe9916e50c41c503a5d0533532e7
Tatiana C. Molina Higgins <73068033+space-schnoodle@users.noreply.github.com>
2022-05-17 16:43:15 -0500

    Manage state when adding a new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./models/Todo";
 
 const App: React.FC = () => {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text }
+    ]);
+  };
+
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
