8701c0c4576dd1b4dc12a8a332c86afcb3d9b28a
Tatiana C. Molina Higgins <73068033+space-schnoodle@users.noreply.github.com>
2022-05-17 16:01:58 -0500

    set up project

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
+import "./index.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
